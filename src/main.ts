8d46fb6339c076f397664e1919194eab1c8b9921
woodywoodpecker <wout_dm@hotmail.com>
2025-12-04 11:16:37 +0100

    verwijderen inhoud template

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello from {{ name }}!</h1>
+    <h1>{{ name }}!</h1>
   `,
 })
 export class App {
