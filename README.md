0d41eeba82302a2e7d87844e841a94b96d5adb0b
woodywoodpecker <git@stackblitz.com>
2025-12-04 11:06:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# test-Stackblitz
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/woodywoodpecker/test-Stackblitz)
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+  `,
+})
+export class App {
+  name = 'Icomet';
+}
+
+bootstrapApplication(App);
